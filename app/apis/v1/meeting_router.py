0d7fba04ef5b37d320068d8debd4fced1761d124
ji-min0 <k.jimin2002@gmail.com>
2025-10-02 13:44:33 +0900

    feat: day-3 spec api

---
+++ app/apis/v1/meeting_router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+from app.dtos.create_meeting_response import CreateMeetingResponse
+
+edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"], redirect_slashes=False)
+mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"], redirect_slashes=False)
+
+@edgedb_router.post(
+"",
+    description="meeting을 생성합니다."
+)
+async def create_meeting_edgedb() -> CreateMeetingResponse:
+    return CreateMeetingResponse(url_code="abc")
+
+@mysql_router.post(
+    "",
+    description="meeting을 생성합니다."
+)
+async def create_meeting_mysql() -> CreateMeetingResponse:
+    return CreateMeetingResponse(url_code="abc")
