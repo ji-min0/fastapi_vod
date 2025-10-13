321bbde23587d5508c63c32b9807045c7e5dcdfd
ji-min0 <k.jimin2002@gmail.com>
2025-10-13 23:00:58 +0900

    날짜 업데이트 EdgeQL 생성 및 조회 EdgeQL에 컬럼 추가

--- app/queries/meeting/get_meeting_by_url_code_async_edgeql.py
+++ app/queries/meeting/get_meeting_by_url_code_async_edgeql.py
@@ -22,7 +22,7 @@ async def get_meeting_by_url_code(
             """\
         with
             url_code := <str>$url_code,
-        select Meeting { url_code }
+        select Meeting { url_code, start_date, end_date, title, location }  # 컬럼 추가
         filter .url_code = url_code
         limit 1\
         """,
