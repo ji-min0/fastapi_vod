acc0ccdfd610c1139ff99e85e0f8ad8675d85e56
ji-min0 <k.jimin2002@gmail.com>
2025-10-13 18:15:09 +0900

    Add meeting retrieval API and tests using EdgeDB

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
 from app.dtos.get_meeting_response import GetMeetingResponse
-from app.services.meeting_service_edgedb import service_create_meeting_edgedb
+from app.services.meeting_service_edgedb import (
+    service_create_meeting_edgedb,
+    service_get_meeting_edgedb,
+)
 
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
 # mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
@@ -26,7 +30,12 @@ async def api_create_meeting_edgedb() -> CreateMeetingResponse:
     description="meeting 을 조회합니다.",
 )
 async def api_get_meeting_edgedb(meeting_url_code: str) -> GetMeetingResponse:
-    return GetMeetingResponse(url_code="abc")
+    meeting = await service_get_meeting_edgedb(meeting_url_code)
+    if meeting is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail=f"meeting with url_code: {meeting_url_code} not found"
+        )
+    return GetMeetingResponse(url_code=meeting.url_code)
 
 
 # @mysql_router.get(
--- app/services/meeting_service_edgedb.py
+++ app/services/meeting_service_edgedb.py
@@ -4,6 +4,10 @@ from app.queries.meeting.create_meeting_async_edgeql import (
     CreateMeetingResult,
     create_meeting,
 )
+from app.queries.meeting.get_meeting_by_url_code_async_edgeql import (
+    get_meeting_by_url_code,
+)
+from app.queries.meeting.models import FullMeeting
 from app.utils.base62 import Base62
 from app.utils.edge import edgedb_client
 
@@ -13,3 +17,7 @@ async def service_create_meeting_edgedb() -> CreateMeetingResult:
         executor=edgedb_client,
         url_code=Base62.encode(uuid.uuid4().int),
     )
+
+
+async def service_get_meeting_edgedb(meeting_url_code: str) -> FullMeeting | None:
+    return await get_meeting_by_url_code(edgedb_client, url_code=meeting_url_code)
